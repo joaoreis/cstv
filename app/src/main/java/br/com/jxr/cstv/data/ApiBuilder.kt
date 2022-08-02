b0659202434da5693cac23de7930ce6391e0c14e
joao <joao.reis21@gmail.com>
2022-08-02 16:23:31 -0300

    Add auth interceptor

--- app/src/main/java/br/com/jxr/cstv/data/ApiBuilder.kt
+++ app/src/main/java/br/com/jxr/cstv/data/ApiBuilder.kt
@@ -1,5 +1,6 @@
 package br.com.jxr.cstv.data
 
+import br.com.jxr.cstv.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -13,6 +14,7 @@ object ApiBuilder {
     inline fun <reified T> buildApi(baseUrl: String): T {
         val okHttpClient = OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor())
+            .addInterceptor(AuthInterceptor(apiKey = BuildConfig.API_KEY))
             .build()
 
         val moshi = Moshi.Builder()
@@ -27,3 +29,4 @@ object ApiBuilder {
             .create()
     }
 }
+
