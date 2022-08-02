e93e1cc4caa18f82e12abbcee3af44aacff7a835
joao <joao.reis21@gmail.com>
2022-08-02 14:49:50 -0300

    Create Api

---
+++ app/src/main/java/br/com/jxr/cstv/data/ApiBuilder.kt
@@ -0,0 +1,29 @@
+package br.com.jxr.cstv.data
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+object ApiBuilder {
+
+    inline fun <reified T> buildApi(baseUrl: String): T {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .build()
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create()
+    }
+}
---
+++ app/src/main/java/br/com/jxr/cstv/data/PandaScoreApi.kt
@@ -0,0 +1,5 @@
+package br.com.jxr.cstv.data
+
+interface PandaScoreApi {
+
+}
